e4f90784bbdbb3a8c38381ec3f8dbf1ba2e3f57c
md2004sameer <md2004sameer01@gmail.com>
2025-08-05 20:36:47 +0530

    update: modified add.java and test.java

--- src/LinkedList/test.java
+++ src/LinkedList/test.java
@@ -1,8 +1,21 @@
 package LinkedList;
 
 public class test {
+
+    public static void welcome(){
+         System.out.println("Hello World");
+
+    }
     public static void main(String args[]){
-        System.out.println("Hello World");
+       welcome();
     }
     
 }
+// nice now i can push my code on github!!
+// cool
+// git status 
+/*
+git add .
+git commit -m "update: modified add.java and test.java"
+git push
+ */
